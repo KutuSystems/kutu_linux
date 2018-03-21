696caae6a84ca0315816b7085c3b27f2dbab8bda
Greg Smart <greg.smart@kutu.com.au>
2018-03-21 12:56:21 +1100

    updated devicetree for Zybo

---
+++ zybo_build.sh
@@ -0,0 +1,7 @@
+source /opt/Xilinx/Vivado/2017.2/settings64.sh
+source /opt/Xilinx/SDK/2017.2/settings64.sh
+export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+export ARCH=arm
+make zynq_zybo_defconfig
+make uImage LOADADDR=0x00008000
+./scripts/dtc/dtc -I dts -O dtb -o devicetree.dtb ./arch/arm/boot/dts/zynq-Zybo-HDMI.dts
