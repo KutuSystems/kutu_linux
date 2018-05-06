566a9cc59399d22d5c9b8e98c131613a41f944d1
Greg Smart <greg.smart@kutu.com.au>
2018-05-06 12:53:35 +1000

    updated zybo build scripts to support z7

--- zybo_build.sh
+++ zybo_build.sh
@@ -1,7 +1,8 @@
-source /opt/Xilinx/Vivado/2017.2/settings64.sh
-source /opt/Xilinx/SDK/2017.2/settings64.sh
-export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+source /opt/Xilinx/Vivado/2017.4/settings64.sh
+source /opt/Xilinx/SDK/2017.4/settings64.sh
+export CROSS_COMPILE=arm-linux-gnueabihf-
 export ARCH=arm
 make zynq_zybo_defconfig
 make uImage LOADADDR=0x00008000
-./scripts/dtc/dtc -I dts -O dtb -o devicetree.dtb ./arch/arm/boot/dts/zynq-Zybo-HDMI.dts
+./scripts/dtc/dtc -I dts -O dtb -o devicetree-v1.dtb ./arch/arm/boot/dts/zynq-Zybo-HDMI-v1.dts
+./scripts/dtc/dtc -I dts -O dtb -o devicetree-z7.dtb ./arch/arm/boot/dts/zynq-Zybo-HDMI-z7.dts
